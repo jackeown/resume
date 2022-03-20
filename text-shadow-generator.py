f0d54160baee2cfe3eea61f0ee58ec0331c0b093
Jack McKeown <jackamckeown@gmail.com>
2022-03-19 20:21:06 -0400

    fixed bug and changed styles a bit

--- text-shadow-generator.py
+++ text-shadow-generator.py
@@ -8,7 +8,7 @@ n = int(sys.argv[1])
 
 def getColors(i):
     magnitude = 1 - math.sqrt(np.abs(i-50) / 50)
-    magnitude *= 4.5
+    magnitude *= 7
     angle = 2*math.pi * (i/100)
     rot1 = np.exp(angle * 1j)
     rot2 = np.exp((2*math.pi / 3) * 1j)
@@ -20,14 +20,16 @@ def getColors(i):
     return r.real, r.imag, g.real, g.imag, b.real, b.imag
 
 
-for i in np.linspace(1,99, n):
+for i in np.linspace(0,100, n):
     ra,rb, ga,gb, ba,bb = getColors(i)
+    mag = 1 - math.sqrt(np.abs(i-50) / 50)
+    mag *= 2
     print(f"""
     
      {int(i)}% {{
-      text-shadow:{ra:.2f}px {rb:.2f}px 1px red,
-                    {ga:.2f}px {gb:.2f}px 1px green,
-                    {ba:.2f}px {bb:.2f}px 1px blue; 
+      text-shadow:{ra:.2f}px {rb:.2f}px {mag:.2f}px red,
+                    {ga:.2f}px {gb:.2f}px {mag:.2f}px green,
+                    {ba:.2f}px {bb:.2f}px {mag:.2f}px blue; 
      }} 
     
     """)
